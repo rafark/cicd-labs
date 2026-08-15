32d83fa51ff33f1dc8d37c5e35da7ad00671d063
rafark <rafark@users.noreply.github.com>
2026-08-15 20:25:41 +0000

    Deploying to build-preview-pr-26 from @ rafark/cicd-labs@1e3d9b6e6c7bdc156e89943c0a5e35c7b59de6bd 🚀

---
+++ coupons-plus-for-woocommerce.php
@@ -0,0 +1,23 @@
+<?php
+use CouponsPlus\Original\Events\Registrator\EventsRegistrator;
+use CouponsPlus\Original\Installation;
+
+/*
+ * Plugin Name: Coupons+
+ * Plugin URI: couponsplus.app
+ * Description: Next-generation coupon offers engine for WooCommerce. Create advanced deals, smart BOGO offers, and more!
+ * Version: dev
+ * Author: neblabs
+ * Author URI: neblabs.com
+ * Text Domain: coupons-plus-for-woocommerce
+ * Domain Path: /international
+ * Requires at least: 5.8
+ * Requires PHP: 7.4
+ * Requires Plugins: woocommerce
+ * License: GPLv3
+*/
+
+/*
+ * this is another comment: should be ignored!
+ */
+echo "this is a plugin test!";
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+use CouponsPlus\Original\Events\Registrator\EventsRegistrator;
+use CouponsPlus\Original\Installation;
+
+/*
+ * Plugin Name:       Coupons+
+ * Plugin URI:        unknown
+ * Description:       Next-generation coupon offers engine for WooCommerce. Create advanced deals, smart BOGO offers, and more!
+ * Version:           dev
+ * Author:            neblabs
+ * Author URI:        unknown
+ * Text Domain:       coupons-plus-for-woocommerce
+ * Domain Path:       /international
+ * Requires at least: unknown
+ * Requires PHP:      unknown
+ * Requires Plugins:  woocommerce
+ * License: GPLv3
+ */
+
+/*
+ * this is another comment: should be ignored!
+ */
+echo "this is a plugin test!";
