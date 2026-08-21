c84594eac8a02ea69406a4fcb67361c082eb872f
rafark <rafark@users.noreply.github.com>
2026-08-21 01:24:15 +0000

    Deploying to build-preview-pr-51-32436138610 from @ rafark/cicd-labs@7d3e61356d7bc0f06449fc36a23babb94743eaed 🚀

---
+++ playwright.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from '@playwright/test';
+
+const config = defineConfig({
+    testDir: './e2e',
+	globalSetup: require.resolve( './e2e/config/global-setup.js' ),
+    fullyParallel: false,
+    use: {
+        baseURL: process.env.WP_BASE_URL || 'http://localhost:8888',
+        trace: 'on-first-retry',
+        channel: 'chrome',
+    },
+    projects: [
+        {
+        name: 'Google Chrome',
+        use: {
+            channel: 'chrome', // Must be explicitly set inside the project's 'use' block
+        },
+        },
+    ],
+});
+
+export default config; 
