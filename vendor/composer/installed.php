b28c1ed2833ab40ca58e2ba8c1c32f736e7a3ef2
rafark <rafark@users.noreply.github.com>
2026-09-01 03:23:06 +0000

    Deploying to build-preview-pr-initial-build-105-33465992557 from @ rafark/cicd-labs@96f6bf216f211be0a5c0b206c0bdca8f13175a35 🚀

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rafa/cicd-labs',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '96f6bf216f211be0a5c0b206c0bdca8f13175a35',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rafa/cicd-labs' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '96f6bf216f211be0a5c0b206c0bdca8f13175a35',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
